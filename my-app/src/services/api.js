11f08bee25c192507ac9150d24abe80099091253
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-03-25 23:16:56 +0100

    api.js
    endringer her

--- my-app/src/services/api.js
+++ my-app/src/services/api.js
@@ -1,15 +1,26 @@
 //  src/services/Api.js  // 
 
 
-const BASE_URL = 'http://openlibrary.org';
+const BASE_URL = 'https://openlibrary.org';
 
-export const fetchJamesBondBooks = async () => {
-  
-  
-}
+export const fetchBooksByQuery = async (query) => {
+  const response = await fetch(`${BASE_URL}/search.json?q=${encodeURIComponent(query)}&fields=title,author_name,first_publish_year`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  const data = await response.json();
+  return data.docs;
+};
 
+export const fetchJamesNondBooks = () => fetchBooksBuQuerry('James Bond');
+
+
+  
+
+
+/*
 export function fetchJamesBondBooks() {
   return fetch('https://openlibrary.org/search.json?q=James+Bond&fields=title,author_name,publish_date,cover_i')
   .then(response => response.json())
   .then(data => data.docs);
-}
+} */
