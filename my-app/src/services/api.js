6f0d2655c5ae52f9df5ed89d2b874801951eb5bd
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-03-25 15:34:48 +0100

    Styles
    Har laget mappen Styles, der inkluderer det meste fra egenlagde css, egenlagde scss/sass fra bunnen.

--- my-app/src/services/api.js
+++ my-app/src/services/api.js
@@ -1,5 +1,13 @@
 //  src/services/Api.js  // 
 
+
+const BASE_URL = 'http://openlibrary.org';
+
+export const fetchJamesBondBooks = async () => {
+  
+  
+}
+
 export function fetchJamesBondBooks() {
   return fetch('https://openlibrary.org/search.json?q=James+Bond&fields=title,author_name,publish_date,cover_i')
   .then(response => response.json())
