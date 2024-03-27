3dd69dc2cfe9eeacabf8cbde6d492e44ba2f037b
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-03-28 00:21:05 +0100

    index.js
    Oppdateriing

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -3,6 +3,33 @@ import React from 'react';
 import bookCardStyles from '../Styles/BookCard.module.css';
 import containerStyles from '../Styles/Container.module.css';
 import BookCard from '../components/BookCard';
+import { fetchJamesBondBooks } from '../services/api';
+
+export async funstion getStaticProps() {
+  const books = await fetchJamesBondBooks();
+  return {
+    props: {
+      books,
+    },
+  };
+}
+
+// En enkelt Home-komponent blir ekspotert
+export default function Home ({ books }) {
+  return (
+    <div className={containerStyles.container}>
+      {books.map((book, index) => (
+        <BookCard key={index} book={book} className={bookCardStyles.bookCard />
+      }
+      ))}
+    </div>
+  )
+}
+
+
+
+
+
 
 export default function Home({ books }) {
   return (
