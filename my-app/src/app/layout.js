feef00f10c3f9be2e9d29c03ac9b1b33ced2fc4e
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-03-28 17:51:44 +0100

    Opprettet ny mappe
    Utils, ligger på rotnivå på src. Konfigurasjonsdata og ikke en komponent eller side. Lager metadata..js fil der

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,6 +1,7 @@
 // src/app/layout.js 
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { metadata } from './metadata';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ my-app/src/pages/_app.js
@@ -0,0 +1,14 @@
+// src/pages/_app.js 
+
+import Layout from '../app/layout';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
