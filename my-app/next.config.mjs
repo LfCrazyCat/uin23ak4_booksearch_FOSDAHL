4a0dda0bf968c6b5648af6f88f423afca5f2e303
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-03-28 13:46:28 +0100

    Update next.config.mjs
    Skrivefeil, rettet opp her

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['covers.openlibrary.org]'], // Må legge til tillattelse fra Open Library
+    domains: ['covers.openlibrary.org'], // Må legge til tillattelse fra Open Library
   },
 };
 
