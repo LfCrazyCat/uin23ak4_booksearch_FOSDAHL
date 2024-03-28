b17a8f436c2f94f34c00cbe3360bb06ee912a4ee
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-03-28 19:11:05 +0100

    Update SearchBar.js
    Fyllt innholdt i filen

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -6,6 +6,21 @@ const SearchBar = ({ onSearch }) => {
   const [input, setInput] = useState('');
 
   const handleInputChange = (e) => {
-    
-  }
-}
+    const value = e.target.value;
+    setInput(value);
+    if (value.length>= 3) {
+      onSearch(value);
+    }
+  };
+
+  return (
+    <input
+    type="text"
+    placeholder="Søk her..."
+    value={input}
+    onChange={handleInputChange}
+    />
+  );
+};
+
+export default SearchBar;
