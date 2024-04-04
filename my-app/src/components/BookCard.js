9a9810cc182e93ac1077d78e3c915e15566cc142
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-04-04 13:22:13 +0200

    api.js
    Endelig fått til  å inkludere metadata som årstall der boken er publisert

--- my-app/src/components/BookCard.js
+++ my-app/src/components/BookCard.js
@@ -2,14 +2,28 @@ import React from 'react';
 import Image from 'next/image';
 
 const BookCard = ({ book }) => {
+
+  const amazonSearchUrl = book.amazon_id ? `https://www.amazon.com/s?k=${book.amazon_id}` : null;
+  
   return (
     <div>
-      <h3>{book.title}</h3>
-      <p>Forfatter: {book.authorName}</p>
-      <p>Publisert: {book.publishYear}</p>
-      <p>Rating: {book.averageRating}</p>
-      {book.amazonId && (
-        <button onClick={() => window.open(`https://www.amazon.com/s?k=0395082544${book.amazonId}`, '_blank')}>
+       <h3>{book.title}</h3>
+      <p>Forfatter: {book.author_name ? book.author_name.join(', ') : 'Ukjent'}</p>
+      <p>Publisert: {book.first_publish_year}</p>
+      <p>Rating: {book.averageRating ? book.averageRating : 'Ikke tilgjengelig'}</p> {/* Hvis du har rating-data */}
+
+      {book.cover_i && (
+                <Image 
+                src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+                alt={`Omslag av boken ${book.title}`}
+                width={50}
+                height={100}
+                layout="responsive"
+              />
+        
+      )}
+            {book.amazon_id && (
+        <button onClick={() => window.open(`https://www.amazon.com/s?k=${book.amazon_id}`, '_blank')}>
           Søk på Amazon
         </button>
       )}
--- my-app/src/services/api.js
+++ my-app/src/services/api.js
@@ -1,19 +1,19 @@
 // src/services/api.js
 
-const BASE_URL = 'https://openlibrary.org';
+const BASE_URL = 'https://openlibrary.org/search.json';
 
 // Fetcher bøker for en spesifikk  query
 export const fetchBooksByQuery = async (query) => {
-  const url = `${BASE_URL}/search.json?q=${encodeURIComponent(query)}&fields=title,author_name,publish_date,cover_i`;
+  const url = `${BASE_URL}?q=${encodeURIComponent(query)}&fields=title,author_name,first_publish_year,cover_i`;
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
   const data = await response.json();
-  return data.docs;
+  return data.docs; // Returnerer en array av bok-objekter basert på søkefrasen
 };
 
-// Fetcher James Bond bok av spesifikk bruk av fetchBooksByQuery 
+// Eksempel på hvordan du kan eksportere en spesifikk funksjon for å hente James Bond bøker
 export const fetchJamesBondBooks = async () => {
   return fetchBooksByQuery('James Bond');
 };
