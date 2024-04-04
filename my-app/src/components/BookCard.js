1bb5001e0bc7d16b2a4d433c6cb733660ec5c27e
LfCrazyCat <89871713+LfCrazyCat@users.noreply.github.com>
2024-04-04 13:59:58 +0200

    BookCard.js
    Endret og lagt inn spesfikk design for søkeresultat

--- my-app/src/components/BookCard.js
+++ my-app/src/components/BookCard.js
@@ -4,22 +4,28 @@ import Image from 'next/image';
 const BookCard = ({ book }) => {
 
   const amazonSearchUrl = book.amazon_id ? `https://www.amazon.com/s?k=${book.amazon_id}` : null;
-  
+
   return (
-    <div>
+    <div style={{ 
+      maxWidth: '150px',
+      margin: '10px',
+      border: '1px solid #ddd',
+      padding: '10px',
+      boxShadow: '0 2px 4px rgba(0,0,0,0.1)'  }}>
        <h3>{book.title}</h3>
       <p>Forfatter: {book.author_name ? book.author_name.join(', ') : 'Ukjent'}</p>
       <p>Publisert: {book.first_publish_year}</p>
-      <p>Rating: {book.averageRating ? book.averageRating : 'Ikke tilgjengelig'}</p> {/* Hvis du har rating-data */}
+      <p>Rating: {book.averageRating ? book.averageRating : 'Ikke tilgjengelig'}</p> {/* Hvis det foreligger rating-data */}
 
       {book.cover_i && (
+        <div style={{ position: 'relative', width: '100%', height: '200px' }}>
                 <Image 
                 src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
                 alt={`Omslag av boken ${book.title}`}
-                width={50}
-                height={100}
-                layout="responsive"
+                layout="fill"
+                objectFit="cover"
               />
+              </div>
         
       )}
             {book.amazon_id && (
